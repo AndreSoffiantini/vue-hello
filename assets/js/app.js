ccc0cbffbb21c993837b3fa5dc90eaaa9e6ef2b1
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-20 16:15:48 +0200

    Add button to generate an image

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,16 @@ const app = new Vue({
 
     data: {
         message: "Test VueJS",
-        image: "https://picsum.photos/400"
+        image: "https://picsum.photos/400?random=1",
+        random_image: ""
+    },
+
+    methods: {
+        generateImages() {
+
+            this.random_image = "https://picsum.photos/400?random=2";
+
+        }
     }
 
 })
